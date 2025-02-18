b192403794dc56b1df01247645c7d05e1c3d682a
AQLT <alain.quartier@yahoo.fr>
2025-02-18 21:31:16 +0100

    last tc values lty

--- R/plot_lollypop.R
+++ R/plot_lollypop.R
@@ -36,19 +36,25 @@ lollypop.default <- function(
 		pch_points = 16,
 		xlab = "",
 		ylab = "",
+		lty_last_tc = 2,
 		...,
-		sa = NULL){
-	complete_data <- ts.union(object, sa)
-	colnames(complete_data) <- c("tc", "sa")
+		sa = NULL,
+		h = 6){
+
+	tc <- object
+	tc_final <- window(tc, end = time(tc)[length(tc) - h])
+	tc_prov <- window(tc, start = time(tc)[length(tc) - h])
+	complete_data <- ts.union(tc_final, tc_prov, sa)
 	if (is.null(xlim))
 		xlim <- range(time(complete_data))
 	if (is.null(ylim))
 		ylim <- range(window(complete_data, start = xlim[1], end = xlim[2]), na.rm = TRUE)
-	plot(complete_data[, "tc"], type = "l",
+	plot(complete_data[, c("tc_final", "tc_prov")], type = "l",
 		 ylim = ylim,
 		 xlim = xlim,
 		 col = col_tc,
 		 ylab = ylab, xlab = xlab,
+		 lty = c(1, lty_last_tc),
 		 ...)
 	points(complete_data[, "sa"], pch = pch_points,
 		   bg = color_points, cex = cex_points)
@@ -67,11 +73,14 @@ lollypop.tc_estimates <- function(
 		pch_points = 16,
 		xlab = "",
 		ylab = "",
+		lty_last_tc = 2,
 		...){
 	lollypop.default(sa = object[["x"]], object = object[["tc"]], xlim = xlim, ylim = ylim,
 					 col_tc = col_tc, col_sa = col_sa,
 					 color_points = color_points, cex_points = cex_points, pch_points = pch_points,
 					 xlab = xlab, ylab = ylab,
+					 h = bandwidth(object),
+					 lty_last_tc = lty_last_tc,
 					 ...)
 }
 #' @name lollypop
@@ -85,6 +94,7 @@ gglollypop <- function(
 		pch_points = 16,
 		legend_tc = "Trend-cycle",
 		legend_sa = "Seasonally adjusted",
+		lty_last_tc = 2,
 		...) {
 	UseMethod("gglollypop")
 }
@@ -98,17 +108,23 @@ gglollypop.default <- function(
 		pch_points = 16,
 		legend_tc = "Trend-cycle",
 		legend_sa = "Seasonally adjusted",
+		lty_last_tc = 2,
 		...,
-		sa = NULL){
-	complete_data <- ts.union(object, sa)
-	colnames(complete_data) <- c("tc", "sa")
+		sa = NULL,
+		h = 6){
+	tc <- object
+	tc_final <- window(tc, end = time(tc)[length(tc) - h])
+	tc_prov <- window(tc, start = time(tc)[length(tc) - h])
+	complete_data <- ts.union(tc_final, tc_prov, sa)
 	data <- data.frame(time = as.numeric(time(complete_data)),
 					   complete_data)
 	ggplot2::ggplot(data = data, ggplot2::aes(x = time)) +
-		ggplot2::geom_line(ggplot2::aes(y = tc, color = legend_tc)) +
+		ggplot2::geom_line(ggplot2::aes(y = tc_final, color = legend_tc), na.rm = TRUE) +
+		ggplot2::geom_line(ggplot2::aes(y = tc_prov), color = col_tc, lty = lty_last_tc, na.rm = TRUE) +
 		ggplot2::geom_point(ggplot2::aes(y = sa, color = legend_sa), pch = pch_points,
-							size = cex_points) +
-		ggplot2::geom_segment(ggplot2::aes(y = tc, yend = sa), color = col_sa) +
+							size = cex_points, na.rm = TRUE) +
+		ggplot2::geom_segment(ggplot2::aes(y = tc_final, yend = sa), color = col_sa, na.rm = TRUE) +
+		ggplot2::geom_segment(ggplot2::aes(y = tc_prov, yend = sa), color = col_sa, na.rm = TRUE) +
 		ggplot2::scale_color_manual(values = c(col_sa, col_tc)) +
 		ggplot2::theme(legend.title = ggplot2::element_blank()) +
 		ggplot2::labs(x = NULL, y = NULL)
@@ -123,10 +139,13 @@ gglollypop.tc_estimates <- function(
 		pch_points = 16,
 		legend_tc = "Trend-cycle",
 		legend_sa = "Seasonally adjusted",
+		lty_last_tc = 2,
 		...){
-	gglollypop.default(sa = object[["x"]], tc = object[["tc"]],
+	gglollypop.default(sa = object[["x"]], object = object[["tc"]],
 					   col_sa = col_sa, col_tc = col_tc,
 					   color_points = color_points, cex_points = cex_points, pch_points = pch_points,
 					   legend_tc = legend_tc, legend_sa = legend_sa,
+					   h = bandwidth(object),
+					   lty_last_tc = lty_last_tc,
 					   ...)
 }
